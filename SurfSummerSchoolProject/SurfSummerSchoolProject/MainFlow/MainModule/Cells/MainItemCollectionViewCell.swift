e185fb4ab75ac549cf10cb6dc356dbf0eb46e256
Slava <Slava7711240@gmail.com>
2022-08-06 23:18:06 +0300

    Created mainModel for Collection View

---
+++ SurfSummerSchoolProject/SurfSummerSchoolProject/MainFlow/MainModule/Cells/MainItemCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MainItemCollectionViewCell.swift
+//  SurfSummerSchoolProject
+//
+//  Created by Slava Orlov on 06.08.2022.
+//
+
+import UIKit
+
+class MainItemCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
