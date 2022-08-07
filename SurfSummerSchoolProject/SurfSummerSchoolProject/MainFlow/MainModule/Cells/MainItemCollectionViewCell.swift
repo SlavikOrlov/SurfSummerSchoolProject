bb9deed14a915047c69370a8481f1b0813087872
Slava <Slava7711240@gmail.com>
2022-08-07 14:26:08 +0300

    Modified Collection View

--- SurfSummerSchoolProject/SurfSummerSchoolProject/MainFlow/MainModule/Cells/MainItemCollectionViewCell.swift
+++ SurfSummerSchoolProject/SurfSummerSchoolProject/MainFlow/MainModule/Cells/MainItemCollectionViewCell.swift
@@ -9,9 +9,74 @@ import UIKit
 
 class MainItemCollectionViewCell: UICollectionViewCell {
 
+    // MARK: - Constants
+    
+    private enum Constants {
+        static let fillHeartImage = UIImage(named: "heart-fill")
+        static let heartImage = UIImage(named: "heart")
+    }
+    // MARK: - Views
+    
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var imageView: UIImageView!
+    @IBOutlet private weak var favoriteButton: UIButton!
+    
+    // MARK: - Events
+    
+    var didFavoritesTapped: (() -> Void)?
+    
+    // MARK: - Calculated
+    
+    var buttonImage: UIImage? {
+        return isFavorite ? Constants.fillHeartImage : Constants.heartImage
+
+    }
+    // MARK: - Properties
+    
+    var title: String = "" {
+        didSet {
+            titleLabel.text = title
+        }
+    }
+    var image: UIImage? {
+        didSet {
+            imageView.image = image
+        }
+    }
+    var isFavorite: Bool = false {
+        didSet {
+            favoriteButton.setImage(buttonImage, for: .normal)
+        }
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction private func favoriteButton(_ sender: UIButton) {
+        didFavoritesTapped?()
+        isFavorite.toggle()
+    }
+    
+    // MARK: - UICollectionViewCell
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        configureAppearance()
+        
     }
 
+}
+
+// MARK: - Private Methods
+
+private extension MainItemCollectionViewCell {
+    
+    func configureAppearance() {
+        titleLabel.textColor = .black
+        titleLabel.font = .systemFont(ofSize: 12)
+        
+        imageView.layer.cornerRadius = 12
+        
+        favoriteButton.tintColor = .white
+    }
 }
