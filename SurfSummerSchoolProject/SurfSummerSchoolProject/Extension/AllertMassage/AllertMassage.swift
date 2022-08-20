fae9ee58155923e2a14c227edeca8d98c406ca1e
Slava <Slava7711240@gmail.com>
2022-08-20 13:00:56 +0300

    Created addAlertView

---
+++ SurfSummerSchoolProject/SurfSummerSchoolProject/Extension/AllertMassage/AllertMassage.swift
@@ -0,0 +1,38 @@
+//
+//  AllertMassage.swift
+//  SurfSummerSchoolProject
+//
+//  Created by Slava Orlov on 20.08.2022.
+//
+
+import UIKit
+
+func addAlertView(
+    for viewController: UIViewController,
+    text: String,
+    completion: @escaping (UIAlertAction) -> Void) {
+    
+        let alert = UIAlertController(
+            title: "Внимание",
+            message: text,
+            preferredStyle: UIAlertController.Style.alert
+        )
+        
+        let confirmAction = UIAlertAction(
+            title: "Да, точно",
+            style: UIAlertAction.Style.default,
+            handler: completion
+        )
+        
+        let cancelAction = UIAlertAction(
+            title: "Нет",
+            style: UIAlertAction.Style.cancel,
+            handler: nil
+        )
+        
+        alert.addAction(confirmAction)
+        alert.addAction(cancelAction)
+        alert.preferredAction = confirmAction
+        
+        viewController.present(alert, animated: true, completion: nil)
+    }
