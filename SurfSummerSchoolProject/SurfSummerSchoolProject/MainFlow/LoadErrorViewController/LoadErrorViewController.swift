c8bf033b3eece5871c43c12c21bc297fa265785e
Slava <Slava7711240@gmail.com>
2022-08-20 14:15:12 +0300

    Created LoadErrorViewController

---
+++ SurfSummerSchoolProject/SurfSummerSchoolProject/MainFlow/LoadErrorViewController/LoadErrorViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoadErrorViewController.swift
+//  SurfSummerSchoolProject
+//
+//  Created by Slava Orlov on 20.08.2022.
+//
+
+import UIKit
+
+class LoadErrorViewController: UIViewController {
+
+    var reloadButtonAction: ()->Void = {}
+
+    @IBOutlet weak var sadSmileImage: UIImageView!
+    @IBOutlet weak var ErrorMessage: UILabel!
+    @IBAction func reloadButton(_ sender: Any) {
+        reloadButtonAction()
+        self.view.alpha = 0.5
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(sadSmileImage)
+        ErrorMessage.text = "Не удалось загрузить ленту. Обновите экран или попробуйте позже"
+    }
+
+
+    
+}
