82c6828c5d3ace1319d4af5a3004800b7b9e4576
Slava <Slava7711240@gmail.com>
2022-08-20 23:39:17 +0300

    Created AuthorizationViewControlle without logic
    It was custom background, textfields and button

---
+++ SurfSummerSchoolProject/SurfSummerSchoolProject/MainFlow/AuthorizationModule/AuthorizationViewController.swift
@@ -0,0 +1,26 @@
+//
+//  AuthorizationViewController.swift
+//  SurfSummerSchoolProject
+//
+//  Created by Slava Orlov on 20.08.2022.
+//
+
+import UIKit
+
+class AuthorizationViewController: UIViewController {
+
+    @IBOutlet weak var loginTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    @IBAction func loginButtonAction(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
