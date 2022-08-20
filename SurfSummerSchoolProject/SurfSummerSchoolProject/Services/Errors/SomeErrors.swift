6dea6f4b715b25c6e65068247bbf2a75ad0f0549
Slava <Slava7711240@gmail.com>
2022-08-20 13:02:11 +0300

    Created SomeErrors

---
+++ SurfSummerSchoolProject/SurfSummerSchoolProject/Services/Errors/SomeErrors.swift
@@ -0,0 +1,18 @@
+//
+//  SomeErrors.swift
+//  SurfSummerSchoolProject
+//
+//  Created by Slava Orlov on 20.08.2022.
+//
+
+import Foundation
+
+enum SomeErrors: Error {
+    case unknownError
+    case urlWasNotFound
+    case urlComponentWasNotCreated
+    case parametersIsNotValidJsonObject
+    case badRequest([String:String])
+    case notNetworkConnection
+    case unknownServerError
+}
